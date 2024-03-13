b6696d6413a449467738b6dba738f5953fb763a8
ArturKorejev <arturkorejev@gmail.com>
2024-03-13 20:25:33 +0200

    3 exercises from Lesson 4 added

---
+++ ArturKorejev/java/lesson4/HelloWorld.java
@@ -0,0 +1,8 @@
+package lesson4;
+
+public class HelloWorld {
+    public static void main(String[] args)  {
+        System.out.println("First Line");
+        System.out.print("Second Line");
+    }
+}
---
+++ ArturKorejev/java/lesson4/calculation.java
@@ -0,0 +1,18 @@
+package lesson4;
+
+public class calculation  {
+    public static void main(String[] args) {
+        int x;
+        int y;
+        int z;
+        int w;
+            x=2;
+            y=3;
+            z=4;
+            w=-2;
+
+            int answer = ( x - z ) + y * y;
+        System.out.println(answer);
+        System.out.println( w / y + (x * z));;
+    }
+}
