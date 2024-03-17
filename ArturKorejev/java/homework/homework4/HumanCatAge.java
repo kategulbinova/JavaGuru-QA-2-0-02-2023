f6222d05c08a92f1b6051571d4a8c23e179eacee
ArturKorejev <arturkorejev@gmail.com>
2024-03-17 20:21:03 +0200

    Homework 4 comments added

---
+++ ArturKorejev/java/homework/homework4/HumanCatAge.java
@@ -0,0 +1,19 @@
+package homework.homework4;
+
+public class HumanCatAge {
+    public static void main(String[] args) {   // Main method created
+
+        int humanAge = 35;
+        int catAge = 4;                       // Declared variables
+        int catAgeIntoHuman = catAge*7;       // Declared calculation of variable
+
+
+        System.out.print("Human age is ");
+        System.out.println(humanAge);
+        System.out.print("Cat age is ");
+        System.out.println(catAge);
+        System.out.print("Cats age equals ");
+        System.out.print(catAgeIntoHuman);
+        System.out.println(" human ages");     // Print result text
+    }
+}
