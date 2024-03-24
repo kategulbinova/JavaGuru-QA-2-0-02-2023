17d2de6e93446d431847958fce71fa41a1bb7d98
ykireev <yury.81518@gmail.com>
2024-03-24 15:37:01 +0200

    adding homework

---
+++ Yury/java/lesson4/Calculation.java
@@ -0,0 +1,11 @@
+package lesson4;
+
+public class Calculation {
+    public static void main(String[] args) {
+        int answer;
+        int x,y,z;
+                x=2; y=4; z=4;
+        answer=(x-z)+y*y;
+        System.out.println(answer);
+    }
+}
---
+++ Yury/java/lesson4/HelloWorld.java
@@ -0,0 +1,14 @@
+package lesson4;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("I study in Java Guru");
+        System.out.println("And I like it");
+        float myAge;
+                myAge=23;
+        System.out.print("I am ");
+        System.out.print(myAge);
+
+    }
+}
