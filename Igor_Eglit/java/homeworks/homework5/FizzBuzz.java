a558f1ed5061cdff7320455602c7538c88488dae
igor eglit <igeglits@gmail.com>
2024-03-28 17:21:26 +0200

    lesson 5

--- Igor_Eglit/java/homeworks/homework5/FizzBuzz.java
+++ Igor_Eglit/java/homeworks/homework5/FizzBuzz.java
@@ -3,15 +3,15 @@ package homeworks.homework5;
 public class FizzBuzz {
     public static void main(String[] args) {
 
-        for (int i = 1; i < 101; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+        for (int number = 1; number < 101; number++) {
+            if (number % 3 == 0 && number % 5 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (i % 5 == 0) {
+            } else if (number % 5 == 0) {
                 System.out.println("Buzz");
-            } else if (i % 3 == 0) {
+            } else if (number % 3 == 0) {
                 System.out.println("Fizz");
             } else {
-                System.out.println(i);
+                System.out.println(number);
             }
         }
     }
--- Igor_Eglit/java/homeworks/homework5/PrintStars.java
+++ Igor_Eglit/java/homeworks/homework5/PrintStars.java
@@ -2,14 +2,15 @@ package homeworks.homework5;
 
 public class PrintStars {
     public static void main(String[] args) {
-        int starsInTheLine = 16;
-        int linesQuantity = 8;
-        toPrintStars(linesQuantity, starsInTheLine);
+        int columns = 16;
+        int rows = 6;
+        toPrintStars(rows, columns);
     }
+
     private static void toPrintStars(int linesQuantity, int starsInTheLine) {
         for (int i = 0; i < linesQuantity; i++) {
             for (int j = 0; j < starsInTheLine; j++) {
-                System.out.print("*");
+                System.out.print(j % 2 == 0 ? "*" : " ");
             }
             System.out.println();
         }
