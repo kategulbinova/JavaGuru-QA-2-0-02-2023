02444af9915f91c28d4bcc713245691e32f5fa27
Viktoria <v.galinkina@yahoo.de>
2024-03-29 08:58:54 +0100

    Add lesson5 practice

---
+++ ViktoriasDirectory/java/lessons/lesson5/Lesson5PracticeBoolean.java
@@ -0,0 +1,16 @@
+package lessons.lesson5;
+
+public class Lesson5PracticeBoolean {
+    public static void main(String[] args) {
+        boolean x = false;
+        boolean y = true;
+
+        boolean z = !x;
+        z = z ^= x;
+        z = z&&y;
+        z = !z;
+        // z = !( ( !x ^ x) && y );
+        System.out.println(z);
+
+    }
+}
---
+++ ViktoriasDirectory/java/lessons/lesson5/Lesson5PracticeStringWeekdays.java
@@ -0,0 +1,27 @@
+package lessons.lesson5;
+
+public class Lesson5PracticeStringWeekdays {
+    public static void main(String[] args) {
+        int dayNum = 6;
+        String dayOfTheWeek; //Null
+        boolean isWeekend; //true or false
+
+
+        switch (dayNum) {
+            case 1: dayOfTheWeek = "Monday"; isWeekend = false; break;
+            case 2: dayOfTheWeek = "Tuesday"; isWeekend = false; break;
+            case 3: dayOfTheWeek = "Wednesday"; isWeekend = false; break;
+            case 4: dayOfTheWeek = "Thursday"; isWeekend = false; break;
+            case 5: dayOfTheWeek = "Friday"; isWeekend = false; break;
+            case 6: dayOfTheWeek = "Saturday"; isWeekend = true; break;
+            case 7: dayOfTheWeek = "Sunday"; isWeekend = true; break;
+            default: dayOfTheWeek = "Unknown day"; isWeekend = false; break;
+        }
+        if (isWeekend) {
+            System.out.println("Today is " + dayOfTheWeek);
+        } else {
+            System.out.println("Looking forward to the Weekend!");
+
+        }
+    }
+}
