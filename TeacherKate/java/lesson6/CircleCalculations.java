4988d192e4c7afbddbeae7da25470e8d57ffa51a
Kate <kate.gulbinova@gmail.com>
2024-04-03 15:27:19 +0300

    Add diameter calculation method to teacher's folder.

--- TeacherKate/java/lesson6/CircleCalculations.java
+++ TeacherKate/java/lesson6/CircleCalculations.java
@@ -6,6 +6,9 @@ public class CircleCalculations {
         return 2 * Math.PI * radius;
     }
 
+    static double calculateDiameter(double radius) {
+        return 2 * radius;
+    }
     public static void main(String[] args) {
         double radius = 3.8;
         System.out.println( "Circumference for this circle is: " + calculateCircumference(radius) + " cm");
