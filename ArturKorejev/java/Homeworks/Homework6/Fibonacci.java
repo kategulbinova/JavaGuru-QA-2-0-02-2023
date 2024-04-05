4afd36e56cf9ea23547908160ea46e473de641e7
ArturKorejev <arturkorejev@gmail.com>
2024-04-06 00:36:00 +0300

    Homework 6 added

--- ArturKorejev/java/Homeworks/Homework6/Fibonacci.java
+++ ArturKorejev/java/Homeworks/Homework6/Fibonacci.java
@@ -1,4 +1,25 @@
 package Homeworks.Homework6;
 
 public class Fibonacci {
+    public static void main(String[] args) {
+
+        int[] numbers = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int sum, f1 = 0, f2 = 1;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = f1 + f2;
+            f2 = f1;
+            f1 = sum;
+            System.out.print(sum + " ");
+
+
+            //use method «printArray(int[] arr) { … }»
+            //inside the method use «for» loop to print each element of an array
+
+
+
+        }
+    }
 }
+
+
+
