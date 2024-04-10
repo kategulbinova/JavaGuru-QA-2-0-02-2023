88ccb257d52a671946395f825e8152031cd180c3
Viktoria <v.galinkina@yahoo.de>
2024-04-10 15:00:11 +0300

    Lesson7 practice: BankAccount – improved even further

--- ViktoriasDirectory/java/lessons/lesson7/bankAccountManagement/Program.java
+++ ViktoriasDirectory/java/lessons/lesson7/bankAccountManagement/Program.java
@@ -2,11 +2,24 @@ package lessons.lesson7.bankAccountManagement;
 
 public class Program {
     public static void main(String[] args) {
-        BankAccount myAccount = new BankAccount();
+        BankAccount anAccount = new BankAccount();
+        BankAccount fullAccount = new BankAccount(25000);
+        BankAccount teacherAccount = new BankAccount("Jekaterina");
+        BankAccount myFullAccount = new BankAccount(10000, "Viktoria");
 
-        myAccount.deposit(100);
-        myAccount.withdraw(150);
-        double myBalance = myAccount.checkBalance();
+        double balance = anAccount.checkBalance();
+        System.out.println(anAccount.getAccountHolderName());
 
+        balance = fullAccount.checkBalance();
+        System.out.println(fullAccount.getAccountHolderName());
+
+        balance = teacherAccount.checkBalance();
+        System.out.println(teacherAccount.getAccountHolderName());
+
+        System.out.println(myFullAccount.getAccountHolderName());
+        double myBalance = myFullAccount.checkBalance();
+        myFullAccount.deposit(100);
+        myFullAccount.withdraw(150);
+        myBalance = myFullAccount.checkBalance();
     }
 }
