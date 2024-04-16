7a010b3521877f50ee6e4dca948e2a38d9619779
Hawajii <kojudgi@gmail.com>
2024-04-16 19:00:09 +0300

    homework5

---
+++ Sergey/javacode/homework5/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package homework5;
+
+public class MultiplicationTable {
+    public static void main (String[] args) {
+
+        int MultiTable=2;
+
+        for ( int i=1; i<=10; i++){
+            System.out.println("2"+"x"+ i + "=" + MultiTable*i);
+
+        }
+
+    }
+}
