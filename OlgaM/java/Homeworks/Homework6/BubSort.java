82d370c16beb7b14500a8b96a0f64083961ffb64
botnursery <botnursery@gmail.com>
2024-04-21 21:51:28 +0300

    Homework6 with methods

--- OlgaM/java/Homeworks/Homework6/BubSort.java
+++ OlgaM/java/Homeworks/Homework6/BubSort.java
@@ -1,5 +1,6 @@
-class BubSort 
-{
+package Homeworks.Homework6;
+
+public class BubSort {
     public static void main( String [] args )
     {
         int[] bubsor = { 45, 87, 23, 2, -15, 67,1005, -1235, -1076652, 0};
@@ -8,33 +9,34 @@ class BubSort
         System.out.println("Initial array of langth " + bubsor.length);
         boolean sorted = false;
         int runs = -1;
-		int chops = 0;
-        
+        int chops = 0;
+
         while(!sorted){
-        	sorted = true;
-        	runs = runs+1;
-        
-	        for ( int i=0; i< bubsor.length-1; i++){
-	        	if( bubsor[i] > bubsor[i+1]){
-	        		chops = chops+1;
-					sorted = false;
-	        		int tmp= bubsor[i];
-	        		bubsor[i] = bubsor [i+1];
-	        		bubsor [i+1]= tmp;
-	        	
-	        	printArray(bubsor);
-	        	System.out.println("");
-	        	}
-			 }
+            sorted = true;
+            runs = runs+1;
+
+            for ( int i=0; i< bubsor.length-1; i++){
+                if( bubsor[i] > bubsor[i+1]){
+                    chops = chops+1;
+                    sorted = false;
+                    int tmp= bubsor[i];
+                    bubsor[i] = bubsor [i+1];
+                    bubsor [i+1]= tmp;
+
+                    printArray(bubsor);
+                    System.out.println("");
+                }
+            }
         }
         //printArray(bubsor);
         //System.out.println("");
         System.out.println("Array sorted in "+ runs +" runs" + " and " + chops + " chops");
-	} 
+    }
 
- 	public static void printArray( int[] myarr){
- 	    for (int i : myarr){
-   	  System.out.print( i +" ");
-   	  }
- 	}
+    public static void printArray( int[] myarr){
+        for (int i : myarr){
+            System.out.print( i +" ");
+        }
+    }
 }
+
