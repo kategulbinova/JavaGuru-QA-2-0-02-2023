1b1f777461390e99d319aa90d2e51df206c8ff04
Kate <kate.gulbinova@gmail.com>
2024-04-30 22:25:38 +0300

    Amended build.gradle & added MyFirstTest file

--- src/test/java/teacher/MyFirstTest.java
+++ src/test/java/teacher/MyFirstTest.java
@@ -1,4 +1,19 @@
 package teacher;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MyFirstTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(); // read about interfaces
+        driver.get("https://www.draugiem.lv");
+        WebElement registretiesLink = driver.findElement(By.id("loginRegister"));
+        WebElement emailInput = driver.findElement(By.id("email"));
+        emailInput.sendKeys("My email");
+        driver.quit();
+    }
 }
