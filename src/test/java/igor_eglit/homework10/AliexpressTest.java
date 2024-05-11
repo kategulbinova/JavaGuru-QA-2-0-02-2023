8eeabe0f34369c158d4c6fb5b3bd71a124f9c39d
igor eglit <igeglits@gmail.com>
2024-05-11 16:57:57 +0300

    lesson 10 hw

--- src/test/java/igor_eglit/homework10/AliexpressTest.java
+++ src/test/java/igor_eglit/homework10/AliexpressTest.java
@@ -1,9 +1,12 @@
-package homeworks.homework10;
+package igor_eglit.homework10;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 class AliexpressTest {
     public static void main(String[] args) {
@@ -13,8 +16,20 @@ class AliexpressTest {
 
         WebElement searchField = driver.findElement(By.id("search-words"));
         searchField.sendKeys("tattoo");
+        System.out.println("Search field filled");
 
         WebElement searchButton = driver.findElement(By.className("search--submit--2VTbd-T"));
         searchButton.click();
+        System.out.println("Search button clicked");
+
+        List<WebElement> searchResultsGallery = driver.findElements(By.className("multi--container--1UZxxHY"));
+        System.out.println(searchResultsGallery.size() + " search results found");
+
+        //Optional: print out all the links of the List using custom method
+        searchResultsGallery.stream()
+                .map(element -> element.getAttribute("href"))
+                .forEach(System.out::println);
+
+        driver.quit();
     }
 }
