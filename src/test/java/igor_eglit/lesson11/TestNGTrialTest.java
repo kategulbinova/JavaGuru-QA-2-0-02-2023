f5cc28c5594b942a49406b8ea8f3b3f008caaab5
igor eglit <igeglits@gmail.com>
2024-05-19 18:45:43 +0300

    lesson 11 lesson / HWs

---
+++ src/test/java/igor_eglit/lesson11/TestNGTrialTest.java
@@ -0,0 +1,11 @@
+package igor_eglit.lesson11;
+
+import org.testng.annotations.Test;
+
+class TestNGTrialTest {
+
+    @Test
+    void myFirstTest(){
+        System.out.println("Hello, TestNG!");
+    }
+}
