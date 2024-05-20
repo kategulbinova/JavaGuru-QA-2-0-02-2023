8a19b2d0d4f0249120ce257738af4ad7fb48a025
Viktoria <v.galinkina@yahoo.de>
2024-05-20 10:36:14 +0200

    Corrected Homework11 and optional part added.

--- src/test/java/viktoria/homeworks/homework11/JanisRozeLoginTest.java
+++ src/test/java/viktoria/homeworks/homework11/JanisRozeLoginTest.java
@@ -2,6 +2,7 @@ package viktoria.homeworks.homework11;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,15 +18,22 @@ import java.time.Duration;
 public class JanisRozeLoginTest {
     private WebDriver driver;
     private Actions action;
-    private int waitTime = 10;
+    private WebDriverWait wait;
+    private JavascriptExecutor jse;
+
+    // Test data definition & initialization
+    private String testMail = "test_email";
+    private String testPassword = "test_pass";
 
     @BeforeTest
     public void setUp() {
-        // Initialize ChromeDriver
+        // Initialize ChromeDriver & actions
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         action = new Actions(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        jse = (JavascriptExecutor) driver;
     }
 
     @Test
@@ -33,27 +41,31 @@ public class JanisRozeLoginTest {
         // Open the JanisRoze website
         driver.get("https://www.janisroze.lv/");
 
-        // Hover mouse on "User profile" icon
+        // Hover mouse on "User profile" icon and choose "Login" option
         WebElement userProfileIcon = driver.findElement(By.xpath("//ul[@class = 'account-dropdown long']"));
         action.moveToElement(userProfileIcon).perform();
-
-        // Click on "Login"
         WebElement loginLink = driver.findElement(By.xpath("//li[@class = ' last']"));
         loginLink.click();
 
         // Wait for Login page to appear
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(waitTime));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
+         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
 
         // Fill user credentials & log in
         WebElement usernameInput = driver.findElement(By.id("email"));
-        usernameInput.sendKeys("your_email");
+        usernameInput.sendKeys(testMail);
 
         WebElement passwordInput = driver.findElement(By.id("pass"));
-        passwordInput.sendKeys("your_password");
+        passwordInput.sendKeys(testPassword);
 
         WebElement loginButton = driver.findElement(By.id("send2"));
         loginButton.click();
+
+        // Scroll down until the footer is visible
+        WebElement footer = driver.findElement(By.className("footer-copyright"));
+        jse.executeScript("arguments[0].scrollIntoView(true);", footer);
+
+        // Additional wait to ensure the footer is visible
+        wait.until(ExpectedConditions.visibilityOf(footer));
     }
 
     @AfterTest
