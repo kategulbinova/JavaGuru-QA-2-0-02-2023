98d92e227199ce6ea96566f97bbc7441512077ff
Kate <kate.gulbinova@gmail.com>
2024-05-20 14:42:43 +0300

    TestNG example

---
+++ src/test/java/teacher/TestNGTrialTest.java
@@ -0,0 +1,10 @@
+package teacher;
+
+import org.testng.annotations.Test;
+
+public class TestNGTrialTest {
+    @Test
+    public void MyFirstTest() {
+        System.out.println("Hello, TestNG!");
+    }
+}
