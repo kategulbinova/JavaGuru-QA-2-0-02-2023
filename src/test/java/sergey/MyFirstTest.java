7b4b4957eea9a090b0fd71f898593b8632e615ed
Hawajii <kojudgi@gmail.com>
2024-05-21 18:17:15 +0300

    homework9

---
+++ src/test/java/sergey/MyFirstTest.java
@@ -0,0 +1,16 @@
+package sergey;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
+
+public class MyFirstTest {
+    public static void main (String[] args){
+        chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.quit();
+    }
+}
