237c1da81a86bc0e190f40a2f20dc43c6051fac9
igor eglit <igeglits@gmail.com>
2024-06-11 21:23:36 +0300

    update test

--- src/test/java/igor_eglit/lesson14/NewBranch.java
+++ src/test/java/igor_eglit/lesson14/NewBranch.java
@@ -3,6 +3,6 @@ package igor_eglit.lesson14;
 public class NewBranch {
 
     public static void main(String[] args) {
-        System.out.println("New Branch Test");
+        System.out.println("New Branch Test test");
     }
 }
