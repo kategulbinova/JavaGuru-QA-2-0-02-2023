e7436bff168de0aa9bbb42815f6961a3814ff863
Hawajii <kojudgi@gmail.com>
2024-06-22 23:05:07 +0300

    homework12

--- src/test/java/sergey/homework11/JanisRozeLoginTest.java
+++ src/test/java/sergey/homework11/JanisRozeLoginTest.java
@@ -6,51 +6,58 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class JanisRozeLoginTest {
 
-    private WebDriver driver;
 
-    @BeforeClass
-    public void setUp() {
+        private WebDriver driver;
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.janisroze.lv/");
-    }
 
-    @Test
-    public void LoginTest() {
-        WebElement profileLink = driver.findElement(By.className("account-dropdown"));
-        Actions actions = new Actions(driver);
-        actions.moveToElement(profileLink).build().perform();
-        try {
-                wait(2000);
-        } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        @BeforeClass
+        public void setUp() {
 
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://www.janisroze.lv");
+            WebDriverWait wait;
+            wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         }
 
+        @Test
 
-        WebElement logIn = driver.findElement(By.name("Ielogoties"));
-        logIn.click();
+        public void loginTest() {
 
-        WebElement emailField = driver.findElement(By.id("email"));
-        WebElement passwordField = driver.findElement(By.id("pass"));
-        WebElement loginButton = driver.findElement(By.id("send2"));
 
-        emailField.sendKeys("hawaji@gmail.com");
-        passwordField.sendKeys("123456");
-        loginButton.click();
-    }
+            WebElement profileLink = driver.findElement(By.className("account-dropdown"));
+            Actions actions = new Actions(driver);
+            actions.moveToElement(profileLink).build().perform();
 
-    @AfterClass
-    public void tearDown(){
-        driver.quit();
-    }
+            WebElement logIn = driver.findElement(By.xpath("//*[@id=\"header-account\"]/ul/li/ul/li[3]/a"));
+            logIn.click();
+
+            WebElement emailField = driver.findElement(By.id("email"));
+            WebElement passwordField = driver.findElement(By.id("pass"));
+            WebElement loginButton = driver.findElement(By.id("send2"));
+
+            emailField.sendKeys("hawaji@gmail.com");
+            passwordField.sendKeys("123456");
+            actions.doubleClick(loginButton).perform();
+        }
+
+        @AfterClass
+
+        public void tearDown() {
+
+            driver.quit();
+        }
 }
+
 
 
